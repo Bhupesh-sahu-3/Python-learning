e2872db317445303a0ac588dc90495c8d705db98
Bhupesh Sahu <bhupesh.rv3@gmail.com>
2026-02-06 21:50:52 +0530

    Add numerical representation file

---
+++ Basics/numericalRepresentation.py
@@ -0,0 +1,7 @@
+print(0o123) # octal representation of 123, converts to 83 in decimal
+print(0x123) # hexadecimal representation of 123, converts to 291 in decimal
+print(0b1010)  # binary representation of 10, converts to 10 in decimal
+print(3e4) # scientific notation, converts to 30000.0 in decimal i.e. 3*10^4
+print(3e-4) # scientific notation, converts to 0.0003 in decimal i.e. 3*10^-4
+
+
