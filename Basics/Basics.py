63a408a7c9d4ef14a77b2fb31076327e453935b2
Bhupesh Sahu <bhupesh.rv3@gmail.com>
2026-02-07 16:46:15 +0530

    modify Basics.py

--- Basics/Basics.py
+++ Basics/Basics.py
@@ -3,4 +3,15 @@ print('hii')
 print('Hii, I\'m a Student')
 print('Hii\nHow are you?')
 
+text='hello world'
+print(text) # prints 'hello world', the entire string
+print(text[0]) # prints 'h', the first character of the string
+print(text[6]) # prints 'w', the seventh character of the string
+print(text[-1]) # prints 'd', the last character of the string
+print(text[0:5]) # prints 'hello', characters from index 0 to 4 (5 is exclusive)
+print(text[6:]) # prints 'world', characters from index 6 to the end of the string
+print(text[:5]) # prints 'hello', characters from the beginning of the string to index 4 (5 is exclusive)
+print(text[::2]) # prints 'hlool', every second character of the string
+print(text[::-1]) # prints 'dlrow olleh', the string in reverse order
+print(text[::-2]) # prints 'drwolh', every second character of the string in reverse order
 
